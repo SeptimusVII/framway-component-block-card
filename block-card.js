e05c314a0fd4c06daa46fa5d5fb4e96883a53bd9
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-05-23 15:42:39 +0200

    bump to 1.0.2

--- block-card.js
+++ block-card.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var BlockCard = Object.getPrototypeOf(app).BlockCard = new app.Component("block-card");
     // BlockCard.debug = true;
     BlockCard.createdAt      = "2.0.0";
-    BlockCard.lastUpdate     = "2.2.1";
-    BlockCard.version        = "1.0.1";
+    BlockCard.lastUpdate     = "2.3.0";
+    BlockCard.version        = "1.0.2";
     // BlockCard.factoryExclude = true;
     // BlockCard.loadingMsg     = "This message will display in the console when component will be loaded.";
     // BlockCard.requires       = [];
