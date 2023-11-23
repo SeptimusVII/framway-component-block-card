5f97a0b3ddfad05674a69625821425d9aeaab039
Quentin Vansteene <vansteene.quentin@gmail.com>
2023-11-23 17:01:09 +0100

    update component to 2.4.3 new standards on img-container

--- block-card.js
+++ block-card.js
@@ -2,8 +2,8 @@ module.exports = function(app){
     var BlockCard = Object.getPrototypeOf(app).BlockCard = new app.Component("block-card");
     // BlockCard.debug = true;
     BlockCard.createdAt      = "2.0.0";
-    BlockCard.lastUpdate     = "2.4.0";
-    BlockCard.version        = "1.0.3";
+    BlockCard.lastUpdate     = "2.4.3";
+    BlockCard.version        = "1.0.4";
     // BlockCard.factoryExclude = true;
     // BlockCard.loadingMsg     = "This message will display in the console when component will be loaded.";
     // BlockCard.requires       = [];
